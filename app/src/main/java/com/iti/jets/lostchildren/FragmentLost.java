df79ffc3497fd2b10c399918c16920ec25330c23
Ahmed Ali <ahmedali31720@gmail.com>
2018-06-12 04:34:16 -0700

    HomeScreen

---
+++ app/src/main/java/com/iti/jets/lostchildren/FragmentLost.java
@@ -0,0 +1,8 @@
+package com.iti.jets.lostchildren;
+
+/**
+ * Created by Ahmed Ali on 6/10/2018.
+ */
+
+public class FragmentLost {
+}
---
+++ app/src/main/java/com/iti/jets/lostchildren/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.iti.jets.lostchildren;
+
+/**
+ * Created by Ahmed Ali on 6/11/2018.
+ */
+
+public class MyAdapter {
+}
